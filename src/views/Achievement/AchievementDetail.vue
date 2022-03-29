42247a51be6122e5078de9fee62872a641177600
xiaoyu <7547189+xytoki@users.noreply.github.com>
2022-03-29 18:28:50 +0800

    ✨ feat(achievement): show prev on hidden quests

--- src/views/Achievement/AchievementDetail.vue
+++ src/views/Achievement/AchievementDetail.vue
@@ -5,21 +5,26 @@
         :z-index="111"
         :width="500"
         :custom-class="$style.detailDialog"
-        @close="$emit('close')"
+        @closed="$emit('close')"
     >
         <div :class="$style.trigger">
             <div class="trigger-type">
                 <b>触发方式：</b>
-                <span>{{
-                    triggerText[achievement?.trigger.type || '未知'] || achievement?.trigger.type || '未知'
-                }}</span>
+                <span>
+                    {{ triggerText[achievement?.trigger.type || '未知'] || achievement?.trigger.type || '未知'
+                    }}{{ hasHiddenOperation ? '后进行特殊操作' : '' }}
+                </span>
             </div>
-            <ul v-if="achievement?.trigger.task" class="task-type">
-                <li v-for="i in achievement.trigger.task" :key="i.questId">
-                    <span class="badge" :class="i.type">{{ taskType[i.type] || i.type || '主线任务' }}</span>
+            <ul v-if="tasks.length > 0" class="task-type">
+                <li v-for="i in tasks" :key="i.questId">
+                    <span v-for="j in i.badges" :key="j" :class="j" class="badge">
+                        {{ taskType[j] || j || '主线任务' }}
+                    </span>
                     <span class="name">{{ amos[i.name] }}</span>
                 </li>
             </ul>
+            <div v-if="showNoTask" class="no-task">触发该成就的任务不存在或均为无名隐藏任务</div>
+            <div class="desc">触发条件为程序自动提取 请以实际游戏或攻略为准</div>
         </div>
     </el-dialog>
 </template>
@@ -27,7 +32,7 @@
 <script lang="ts">
 import { i18n } from '@/i18n'
 import { Achievement } from '@/typings/Achievement'
-import { defineComponent, PropType, toRef } from 'vue'
+import { defineComponent, PropType, toRef, computed } from 'vue'
 
 export default defineComponent({
     props: {
@@ -37,7 +42,7 @@ export default defineComponent({
         },
     },
     emits: ['close'],
-    setup() {
+    setup(props) {
         const triggerText = {
             FINISH_QUEST_OR: '完成以下任务之一',
             FINISH_QUEST_AND: '完成以下所有任务',
@@ -46,11 +51,34 @@ export default defineComponent({
         const taskType = {
             WQ: '世界任务',
             IQ: '每日委托',
+            H: '前置条件',
         } as Record<string, string>
+        const tasks = computed(() => {
+            if (!props.achievement || !props.achievement.trigger.task) return []
+            return props.achievement.trigger.task.map((e) => {
+                return {
+                    ...e,
+                    badges: e.type.split(' '),
+                }
+            })
+        })
+        const hasHiddenOperation = computed(() => {
+            return tasks.value.find((e) => e.badges.includes('H'))
+        })
+        const showNoTask = computed(() => {
+            if (!props.achievement) return []
+            return (
+                tasks.value.length === 0 &&
+                ['FINISH_QUEST_OR', 'FINISH_QUEST_AND'].includes(props.achievement.trigger.type)
+            )
+        })
         return {
             triggerText,
             taskType,
             amos: toRef(i18n, 'amos'),
+            tasks,
+            hasHiddenOperation,
+            showNoTask,
         }
     },
 })
@@ -61,6 +89,7 @@ export default defineComponent({
     max-width: 95%;
 }
 .trigger {
+    margin-top: -20px;
     :global {
         .badge {
             font-size: 12px;
@@ -70,10 +99,24 @@ export default defineComponent({
             margin-right: 5px;
             border-radius: 3px;
             &.IQ {
-                background: #7236e1;
+                background: #8d4be5;
+            }
+            &.H {
+                background: #dd5959;
             }
         }
+        .no-task {
+            padding: 30px;
+            text-align: center;
+        }
 
+        .desc {
+            text-align: center;
+            color: #999;
+            font-size: 12px;
+            margin-top: 15px;
+            margin-bottom: -15px;
+        }
         .task-type li {
             margin-top: 10px;
         }

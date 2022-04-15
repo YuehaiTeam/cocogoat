03ede8ea13dab98d6bf0545100ac584ea7c8d6d7
xiaoyu <7547189+xytoki@users.noreply.github.com>
2022-04-15 14:48:23 +0800

    🌈 data: remove character images

--- src/assets/characters/index.ts
+++
@@ -1,7 +0,0 @@
-const characterImages = require.context('./', true, /(.*?)\.webp$/)
-const characterImagesMap = {} as Record<string, string>
-characterImages.keys().forEach((key) => {
-    const name = key.replace(/^\.\/(.*?)\.webp$/, '$1')
-    characterImagesMap[name] = characterImages(key)
-})
-export default characterImagesMap

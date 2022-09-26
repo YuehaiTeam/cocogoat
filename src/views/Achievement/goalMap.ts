870ea5bec969f7837ebf37207c62a514c00ed09f
xiaoyu <7547189+xytoki@users.noreply.github.com>
2022-09-26 16:00:43 +0800

    ✨ feat: 3.1 achievements

--- src/views/Achievement/goalMap.ts
+++ src/views/Achievement/goalMap.ts
@@ -4,6 +4,9 @@ const goalKeys = {
     31: 'sumeru',
     32: 'mortal-travails-series-iii',
     33: 'meetings-in-outrealm-series-iii',
+    34: 'challenger-series-vi',
+    35: 'sumeru-the-gilded-desert-series-i',
+    36: 'elemental-specialist-series-ii',
 } as Record<number, string>
 achevementsAmos.forEach((cat) => {
     cat.key = goalKeys[cat.id] || cat.key.replace(/_/g, '-') || cat.id.toString()

92078f2cb8eea33bcfae0ffd4c4d2cd704382545
xiaoyu <7547189+xytoki@users.noreply.github.com>
2023-04-25 19:49:51 +0800

    ✨ feat: add client pkg for some other game(s), fix #37

--- src/views/Installer/urls.ts
+++ src/views/Installer/urls.ts
@@ -11,4 +11,12 @@ export const urls = {
         name: '海外',
         url: 'https://sdk-os-static.mihoyo.com/hk4e_global/mdk/launcher/api/resource?channel_id=1&key=gcStgarh&launcher_id=10&sub_channel_id=0',
     },
+    'bh3-cn': {
+        name: '崩坏3（国服）',
+        url: 'https://sdk-static.mihoyo.com/bh3_cn/mdk/launcher/api/resource?channel_id=1&key=SyvuPnqL&launcher_id=4&sub_channel_id=1',
+    },
+    'hkrpg-cn': {
+        name: '崩坏：星穹铁道',
+        url: 'https://api-launcher-static.mihoyo.com/hkrpg_cn/mdk/launcher/api/resource?channel_id=1&key=6KcVuOkbcqjJomjZ&launcher_id=33&sub_channel_id=1',
+    },
 } as Record<string, { name: string; url: string }>

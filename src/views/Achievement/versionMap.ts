a010ef6a8bb26944426bfce7f71ef78efcc0a039
xiaoyu <7547189+xytoki@users.noreply.github.com>
2026-01-14 13:53:14 +0800

    ✨ feat: 6.3 vmap

--- src/views/Achievement/versionMap.ts
+++ src/views/Achievement/versionMap.ts
@@ -9,6 +9,7 @@ const versionMap = Object.entries(versionMeta).reduce(
     {} as Record<number, number>,
 )
 export const versionDateMap = {
+    6.3: new Date('2026-01-14'),
     6.2: new Date('2025-12-03'),
     6.1: new Date('2025-10-22'),
     6.0: new Date('2025-09-10'),
